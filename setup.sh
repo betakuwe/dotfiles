0442164ad2fd694a40e63c1cdcb8b4d6f89508a2
Benjamin Tan <benjamin.tan@bt-wks.klass.dev>
2025-06-24 16:42:07 +0800

    switch to DOOM EMACS

--- setup.sh
+++ setup.sh
@@ -1,18 +1,2 @@
-#!/usr/bin/env sh
-
-set -eu
-
-# Setup emacs
-EMACS_CONFIG="$HOME/.emacs.d"
-PRELUDE_GIT_URL="https://github.com/bbatsov/prelude.git"
-if [ -d $EMACS_CONFIG/.git ] && [ "$(git -C ~/.emacs.d/ config --get remote.origin.url)" = "$PRELUDE_GIT_URL" ]; then
-    echo "Prelude git directory found at $EMACS_CONFIG"
-else
-    echo "Prelude git directory NOT found at $EMACS_CONFIG"
-    rm -rf $EMACS_CONFIG
-    git clone $PRELUDE_GIT_URL $EMACS_CONFIG
-fi
-stow --dotfiles --verbose --target="$EMACS_CONFIG" --restow dot-emacs.d
-
-# Stow .gitconfig
+stow --dotfiles --verbose --target="$HOME/.config/doom" --restow doom
 stow --dotfiles --verbose --target="$HOME" --restow .
