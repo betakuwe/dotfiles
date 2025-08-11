1c2304a60c10d7e0e197ba09f17e19d82cc53595
Benjamin Tan <betakuwe@gmail.com>
2025-08-11 20:33:28 +0800

    better completion

--- dot-emacs.d/post-init.el
+++ dot-emacs.d/post-init.el
@@ -307,6 +307,7 @@
   :hook (magit-post-refresh . diff-hl-magit-post-refresh)
   :config
   (diff-hl-dired-mode 1)
+  (diff-hl-flydiff-mode 1)
   (global-diff-hl-mode))
 
 (use-package phi-search
@@ -331,6 +332,17 @@
   :bind
   ("C-=" . 'er/expand-region)
   ("C-+" . 'er/contract-region))
+
+(use-package cape
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block)
+  (add-hook 'completion-at-point-functions #'cape-history))
 
 (use-package markdown-mode :defer t)
 (use-package toml-mode :defer t)
