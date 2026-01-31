46d4d55d6adb31a9c70ea828a03e2538d1c2e0e2
Benjamin Tan <betakuwe@gmail.com>
2026-01-31 17:46:15 +0800

    some consult errors

--- dot-emacs.d/post-init.el
+++ dot-emacs.d/post-init.el
@@ -220,6 +220,7 @@
 
 (use-package embark-consult
   :ensure t
+  :after (embark consult)
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
 
@@ -311,8 +312,8 @@
    consult-theme :preview-key '(:debounce 0.2 any)
    consult-ripgrep consult-git-grep consult-grep
    consult-bookmark consult-recent-file consult-xref
-   consult--source-bookmark consult--source-file-register
-   consult--source-recent-file consult--source-project-recent-file
+   ;; consult--source-bookmark consult--source-file-register
+   ;; consult--source-recent-file consult--source-project-recent-file
    ;; :preview-key "M-."
    :preview-key '(:debounce 0.4 any))
   (setq consult-narrow-key "<"))
